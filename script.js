fc59a1fd6a9d5eb1a055b23cdc74dc0f5fbef6fc
Lun <153135253+Lunw07@users.noreply.github.com>
2026-07-05 22:24:19 +0100

    Implemented Oauth for Spotify Web API

--- script.js
+++ script.js
@@ -47,6 +47,8 @@ function validatePlaylistID(id){
     }
 }
 
+//------------------------------------------------------------------------------------------------------------------------------------------
+
 urlForm.addEventListener("submit", async (e) => {
     e.preventDefault();
 
@@ -65,7 +67,6 @@ urlForm.addEventListener("submit", async (e) => {
     }
 
     console.log("Playlist ID:", id);
-
 
     const apiUrl = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${id}&maxResults=50&key=${API_KEY}`;
     const res = await fetch(apiUrl);
@@ -95,5 +96,104 @@ urlForm.addEventListener("submit", async (e) => {
     console.log(titles);
 
 });
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+const clientId = "2daccc6066bc41068c6404e16b8600e0";
+const redirectUri = "http://127.0.0.1:5500/main.html";
+
+const scopes = [
+    "playlist-modify-public",
+    "playlist-modify-private"
+];
+
+function generateRandomString (length){
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const values = crypto.getRandomValues(new Uint8Array(length));
+    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+}
+
+async function sha256(text) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(text);
+    return crypto.subtle.digest("SHA-256", data);
+}
+
+function base64encode(input) {
+    return btoa(String.fromCharCode(...new Uint8Array(input)))
+        .replace(/\+/g, "-")
+        .replace(/\//g, "_")
+        .replace(/=+$/, "");
+}
+
+document.getElementById("login").addEventListener("click", async () => {
+
+    const verifier = generateRandomString(64);  
+    localStorage.setItem("verifier", verifier);
+
+    const hashed = await sha256(verifier);
+    const codeChallenge = base64encode(hashed);
+
+    const authUrl = new URL("https://accounts.spotify.com/authorize");
+    const scope = "playlist-modify-public playlist-modify-private";
+
+    const params =  {
+        response_type: 'code',
+        client_id: clientId,
+        scope,
+        code_challenge_method: 'S256',
+        code_challenge: codeChallenge,
+        redirect_uri: redirectUri,
+    }
+
+    authUrl.search = new URLSearchParams(params).toString();
+    window.location.href = authUrl.toString();
+});
+
+
+window.addEventListener("load", () => {
+    if (window.location.search.length > 0) {
+        handleRedirect();
+    }
+});
+
+function handleRedirect(){
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get("code");
+
+    if (code) {
+        exchangeCodeForToken(code);
+    }
+
+}
+
+async function exchangeCodeForToken(code) {
+    const codeVerifier = localStorage.getItem('verifier');
+
+    const url = "https://accounts.spotify.com/api/token";
+    const payload = {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+        client_id: clientId,
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: redirectUri,
+        code_verifier: codeVerifier,
+        }),
+    }
+
+    const body = await fetch(url, payload);
+    const response = await body.json();
+
+    localStorage.setItem('access_token', response.access_token);
+    console.log("TOKEN:", response.access_token);
+
+    // clean URL
+    window.history.replaceState({}, document.title, "/main.html");
+}
+
 
 
